4549d4356f1da9127fd46ffbf5730c5ef18ebe32
Felippe Santana <42714571+FelippeMenezes@users.noreply.github.com>
2025-08-19 20:40:14 -0300

    Remove Campaigns name null false

--- db/migrate/20250819202445_create_campaigns.rb
+++ db/migrate/20250819202445_create_campaigns.rb
@@ -1,7 +1,7 @@
 class CreateCampaigns < ActiveRecord::Migration[7.1]
   def change
     create_table :campaigns do |t|
-      t.string :name, null: false
+      t.string :name
 
       t.timestamps
     end
