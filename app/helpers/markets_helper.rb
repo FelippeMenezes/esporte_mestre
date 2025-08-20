a18af7c23fdf4fdf5103bba0af1a2e42859b12a6
Felippe Santana <42714571+FelippeMenezes@users.noreply.github.com>
2025-08-20 19:58:42 -0300

    Responsive and select players

--- app/helpers/markets_helper.rb
+++ app/helpers/markets_helper.rb
@@ -1,2 +1,14 @@
 module MarketsHelper
+  def sortable(column, title = nil)
+    title ||= column.titleize
+    direction = (column == sort_column && sort_direction == "asc") ? "desc" : "asc"
+
+    # Add an arrow icon to the currently sorted column header
+    icon = ""
+    if column == sort_column
+      icon = sort_direction == "asc" ? " <i class='fas fa-sort-up'></i>" : " <i class='fas fa-sort-down'></i>"
+    end
+
+    link_to((title + icon).html_safe, { sort: column, direction: direction }, { class: "text-dark text-decoration-none" })
+  end
 end
