b02319a0dedcc4056ec580fa66cd011c3c6ed977
Felippe Menezes <felippemenezes1982@gmail.com>
2025-10-27 17:34:35 -0300

    Change no matches alert into sweet alert

---
+++ app/javascript/controllers/no_matches_controller.js
@@ -0,0 +1,27 @@
+import { Controller } from "@hotwired/stimulus"
+import Swal from "sweetalert2"
+
+export default class extends Controller {
+  connect() {
+    const newMatchPath = this.element.dataset.noMatchesNewMatchPath;
+    Swal.fire({
+      title: "Nenhuma partida encontrada",
+      html: `Seu time ainda não jogou nenhuma partida.</br> <a href="${newMatchPath}">Criar uma nova partida</a> agora!`,
+      icon: "info",
+      confirmButtonText: "Entendi",
+      confirmButtonColor: '#198754',
+      toast: true,
+      timer: 10000,
+      showConfirmButton: true,
+      customClass: {
+        container: 'swal-container',
+        popup: 'swal-popup',
+        title: 'swal-title',
+        htmlContainer: 'swal-text',
+        icon: 'swal-icon',
+        actions: 'swal-actions',
+        confirmButton: 'swal-confirm-button',
+      }
+    });
+  }
+}
