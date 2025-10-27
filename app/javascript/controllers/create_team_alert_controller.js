f60a680d97cfe800552ff6b35139f1fe7b086148
Felippe Menezes <felippemenezes1982@gmail.com>
2025-10-26 22:05:09 -0300

    Update create team sweet alert

---
+++ app/javascript/controllers/create_team_alert_controller.js
@@ -0,0 +1,33 @@
+import { Controller } from "@hotwired/stimulus"
+import Swal from "sweetalert2"
+
+export default class extends Controller {
+  connect() {
+    Swal.fire({
+      title: "O que acontece quando você cria um time:",
+      html: `
+        <ul style="text-align: left;">
+          <li>Uma nova campanha será criada exclusivamente para você</li>
+          <li>9 times rivais serão gerados automaticamente</li>
+          <li>Cada time (incluindo o seu) receberá 15 jogadores</li>
+          <li>Você começará com um orçamento de $10.000.000</li>
+        </ul>
+      `,
+      icon: "info",
+      confirmButtonText: "Entendi",
+      confirmButtonColor: '#198754',
+      toast: true,
+      timer: 15000,
+      showConfirmButton: true,
+      customClass: {
+        container: 'swal-container',
+        popup: 'swal-popup',
+        title: 'swal-title',
+        htmlContainer: 'swal-text',
+        icon: 'swal-icon',
+        actions: 'swal-actions',
+        confirmButton: 'swal-confirm-button',
+      }
+    });
+  }
+}
