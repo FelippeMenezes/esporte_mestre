7301697556f6760227140ad4be836df05ee6e774
Felippe Menezes <felippemenezes1982@gmail.com>
2025-10-27 22:00:49 -0300

    Update simulation info

--- app/javascript/controllers/simulation_info_controller.js
+++ app/javascript/controllers/simulation_info_controller.js
@@ -3,30 +3,36 @@ import Swal from "sweetalert2"
 
 export default class extends Controller {
   connect() {
-    Swal.fire({
-      title: "Como funciona a simulação:",
-      html: `
-        <ul style="text-align: left;">
-          <li>O resultado é calculado baseado no nível médio dos jogadores</li>
-          <li>Há um fator aleatório para tornar o jogo mais emocionante</li>
-          <li>A partida será jogada automaticamente quando você confirmar</li>
-        </ul>
-      `,
-      icon: "info",
-      confirmButtonText: "Entendi",
-      confirmButtonColor: '#198754',
-      toast: true,
-      timer: 10000,
-      showConfirmButton: true,
-      customClass: {
-        container: 'swal-container',
-        popup: 'swal-popup',
-        title: 'swal-title',
-        htmlContainer: 'swal-text',
-        icon: 'swal-icon',
-        actions: 'swal-actions',
-        confirmButton: 'swal-confirm-button',
-      }
-    });
+    // Check if the user has already seen the simulation info alert
+    if (!localStorage.getItem('simulationInfoShown')) {
+      Swal.fire({
+        title: "Como funciona a simulação:",
+        html: `
+          <ul style="text-align: left;">
+            <li>O resultado é calculado baseado no nível médio dos jogadores</li>
+            <li>Há um fator aleatório para tornar o jogo mais emocionante</li>
+            <li>A partida será jogada automaticamente quando você confirmar</li>
+          </ul>
+        `,
+        icon: "info",
+        confirmButtonText: "Entendi",
+        confirmButtonColor: '#198754',
+        toast: true,
+        timer: 10000,
+        showConfirmButton: true,
+        customClass: {
+          container: 'swal-container',
+          popup: 'swal-popup',
+          title: 'swal-title',
+          htmlContainer: 'swal-text',
+          icon: 'swal-icon',
+          actions: 'swal-actions',
+          confirmButton: 'swal-confirm-button',
+        }
+      });
+
+      // Set the flag in localStorage to prevent showing again
+      localStorage.setItem('simulationInfoShown', 'true');
+    }
   }
 }
