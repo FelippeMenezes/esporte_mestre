21185c89a7e1df98b58ae3020f71d261f5f39006
fsantana-springbot <fsantana@springbot.com>
2025-12-25 21:48:17 -0300

    Koyeb deploy

--- bin/render-build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# exit on error
-set -o errexit
-
-bundle install
-bundle exec rake assets:clobber
-SECRET_KEY_BASE_DUMMY=1 bundle exec rake assets:precompile
-bundle exec rake assets:clean
